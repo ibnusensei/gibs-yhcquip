b7836cd681cafd2afacd5762be21bbbe065bbbf2
daffaharizal <harizal.daffa46@gmail.com>
2023-03-13 15:26:28 +0800

    Create Article Models

---
+++ resources/views/admin/article/index.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h1 class="container-fluid">Article</h1>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\GalleryController as AdminGalleryController;
+use App\Http\Controllers\Admin\ArticleController as AdminArticleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,7 +36,7 @@ Route::middleware('auth')->group(function () {
 Route::prefix('admin')->middleware(['role:admin'])->name('admin.')->group(function() {
     Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
     Route::resource('gallery', AdminGalleryController::class);
-
+    Route::get('article', [AdminArticleController::class, 'index'])->name('article');
     Route::post('gallery-image/{gallery}', [AdminGalleryController::class, 'imageStore'])->name('gallery.image.store');
     Route::delete('gallery-image/{gallery}', [AdminGalleryController::class, 'imageDestroy'])->name('gallery.image.destroy');
 });
