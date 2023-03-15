7475833934f62526cee7ba5db0aedfb166bcab85
daffaharizal <harizal.daffa46@gmail.com>
2023-03-15 10:50:19 +0800

    Add article section to sidebar

--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -1,3 +1,68 @@
 <x-app-layout>
-    <h1 class="container-fluid">Article</h1>
+
+    <!-- Content -->
+    <div class="content container-fluid">
+        <!-- Page Header -->
+        <div class="page-header">
+            <div class="row align-items-center">
+                <div class="col">
+                    <h1 class="page-header-title">Gallery</h1>
+                </div>
+                <!-- End Col -->
+
+                <div class="col-auto">
+                    <a class="btn btn-primary" href="{{ route('admin.gallery.create') }}">
+                        <i class="bi-plus me-1"></i> Create
+                    </a>
+                </div>
+                <!-- End Col -->
+            </div>
+            <!-- End Row -->
+        </div>
+        <!-- End Page Header -->
+
+        <div class="card border-primary">
+            <div class="card-body">
+                <h4 class="card-title">Data Gallery</h4>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Publish</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-align-middle">
+                            <tr>
+                                <td colspan="2" class="text-center bg-primary text-white">No Data</td>
+                                <td>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" role="switch"
+                                            id="flexSwitchCheckDefault">
+                                        <label class="form-check-label" for="flexSwitchCheckDefault">Publish</label>
+                                    </div>
+                                </td>
+                                <td>
+                                    <a name="" id="" class="btn btn-success btn-sm"
+                                        href="#">Edit</a>
+                                    <a name="" id="" class="btn btn-primary btn-sm"
+                                        href="#">Show</a>
+                                    <form action="#" method="POST" class="d-inline">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button class="btn btn-danger btn-sm delete-btn">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Content -->
+
 </x-app-layout>
