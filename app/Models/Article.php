a6f53ef6e29d5fa0e365866b6aecb31ea08a8faa
daffaharizal <harizal.daffa46@gmail.com>
2023-03-17 09:36:04 +0800

    Import fill and create two method: category_article and spatieLibrary

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Article extends Model
 {
-    use HasFactory;
+  use HasFactory;
+  protected $fillable = ['slug', 'title', 'description', 'author', 'comment',];
+
+  public function category_article(): BelongsTo
+  {
+    return $this->belongsTo(CategoryArticle::class);
+  }
+
+  public function registerMediaConversions(Media $media = null): void
+  {
+    $this
+      ->addMediaConversion('preview')
+      ->fit(Manipulations::FIT_CROP, 300, 300)
+      ->nonQueued();
+  }
 }
