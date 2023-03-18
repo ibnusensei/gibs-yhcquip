c860b7285139df9b0c58ee87d8db218737c2ca9c
daffaharizal <harizal.daffa46@gmail.com>
2023-03-18 11:03:27 +0800

    Feat: Create category, comment menthod, and update etc

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class ArticleController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   */
-  
+
+  public function category()
+  {
+    return view('admin.article.category');
+  }
+
+  public function comment()
+  {
+    return view('admin.article.comment');
+  }
+
   public function index()
   {
     // Data Articles
@@ -20,57 +29,78 @@ class ArticleController extends Controller
     return view('admin.article.index', compact('articles'));
   }
 
-  /**
-   * Show the form for creating a new resource.
-   */
   public function create()
   {
-    return view('admin.article.form');
+    $url = route('admin.article.store');
+    // ss
+    return view('admin.article.form', compact('url'));
   }
 
-  /**
-   * Store a newly created resource in storage.
-   */
   public function store(Request $request)
   {
-    //
+    $data = $request->validate([
+      'title' => 'required | string',
+      'description' => 'nullable | string',
+      'author' => 'required | string',
+    ]);
+
+    $data['slug'] = Str::slug($request->title);
+
+    $article = Article::create($data);
+    if ($request->hasFile('image')) {
+      $article->addMediaFromRequest('image')->toMediaCollection('image');
+    }
+
+    toast('Create article Success', 'success');
+    return redirect()->route('admin.article.index');
   }
 
-  /**
-   * Display the specified resource.
-   */
   public function show(string $id)
   {
-    //
+    $article = Article::findOrFail($id);
+    return view('admin.article.show', compact('article'));
   }
 
-  /**
-   * Show the form for editing the specified resource.
-   */
+  
   public function edit(string $id)
   {
-    //
+    $article = Article::findOrFail($id);
+    $url = route('admin.article.update', $article);
+
+    return view('admin.article.form', compact('article', 'url'));
   }
 
-  /**
-   * Update the specified resource in storage.
-   */
   public function update(Request $request, string $id)
   {
-    //
+    $article = Article::findOrFail($id);
+    $data = $request->validate([
+      'title' => 'required | string',
+      'description' => 'nullable | string',
+      'author' => 'required | string',
+    ]);
+
+    $data['slug'] = Str::slug($request->title);
+    $article->update($data);
+
+    toast('Your Gallery has been updated!', 'success');
+    return redirect()->route('admin.article.index');
   }
 
   /**
    * Remove the specified resource from storage.
    */
-  public function destroy(string $id)
+  public function destroy(Article $article)
   {
-    //
+    $article->delete();
+    return redirect()->route('admin.article.index');
   }
 
-
-  public function category()
+  public function imageDestroy($id)
   {
-    return view('admin.article.category');
+    $media = Media::findOrFail($id);
+    $media->delete();
+
+    toast('Your Image has been deleted', 'success');
+    return redirect()->back();
   }
 }
