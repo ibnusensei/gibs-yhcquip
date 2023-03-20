a27cebcfaf3a4819b8f6188bf4151cc3c44f2bad
daffaharizal <harizal.daffa46@gmail.com>
2023-03-20 11:49:46 +0800

    Feat: menambahkan fillable dan method untuk kebutuhan relasi

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -15,12 +15,12 @@ class Article extends Model implements HasMedia
   use HasFactory;
   use InteractsWithMedia;
 
-  protected $fillable = ['slug', 'title', 'description', 'author', 'comment', 'category_id'];
+  protected $fillable = ['slug', 'title', 'description', 'author', 'comment'];
 
-  public function categoryArticle(): BelongsTo
-  {
-    return $this->belongsTo(CategoryArticle::class);
-  }
+  // public function category_article(): BelongsTo
+  // {
+  //   return $this->belongsTo(CategoryArticle::class);
+  // }
 
   public function registerMediaConversions(Media $media = null): void
   {
